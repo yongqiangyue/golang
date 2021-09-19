2868b8804db7715a85161a4f05ea10fa0cbb96c7
yueyq <yongqiangyue@yeah.net>
2021-09-19 13:18:49 +0800

    study busyloop

--- examples/busyloop/main.go
+++ examples/busyloop/main.go
@@ -1,11 +1,20 @@
 package main
 
+import (
+	"fmt"
+	"time"
+)
+
+// note: go build -o busyloop main.go
 func main() {
 	go func() {
 		for {
+			fmt.Println("log from go")
+			time.Sleep(time.Duration(2) * time.Second)
 		}
 	}()
 	for {
-
+		fmt.Println("main from go")
+		time.Sleep(time.Duration(1) * time.Second)
 	}
 }
