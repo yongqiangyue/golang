65d000fbc757b6571a8d5bbd2a3c73ead5294fef
yueyq <yongqiangyue@yeah.net>
2021-09-21 10:11:42 +0800

    json unmarshal & marshal

--- examples/interface/main.go
+++ examples/interface/main.go
@@ -1,13 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+)
 
 type IF interface {
 	getName() string
 }
 
 type Human struct {
-	firstName, lastName string
+	FirstName, LastName string
+	Age                 int
+}
+
+// 字符串转struct
+func unmarshal2Struct(humanStr string) *Human {
+	h := Human{}
+	if err := json.Unmarshal([]byte(humanStr), &h); err != nil {
+		fmt.Println("json unmarshal failed.", err)
+	}
+	fmt.Printf("call %p\n", &h)
+	defer fmt.Printf("defer call %p\n", &h)
+	return &h
+}
+
+// struct转字符串
+func marshal2JsonString(h Human) string {
+	h.Age = 37
+	updatedBytes, err := json.Marshal(&h)
+	if err != nil {
+		fmt.Println(err)
+	}
+	return string(updatedBytes)
 }
 
 type Plane struct {
@@ -16,7 +41,7 @@ type Plane struct {
 }
 
 func (h *Human) getName() string {
-	return h.firstName + "," + h.lastName
+	return h.FirstName + "," + h.LastName
 }
 
 func (p Plane) getName() string {
@@ -32,20 +57,28 @@ func (c *Car) getName() string {
 }
 
 func main() {
-	interfaces := []IF{}
-	h := new(Human)
-	h.firstName = "first"
-	h.lastName = "last"
-	interfaces = append(interfaces, h)
-	c := new(Car)
-	c.factory = "benz"
-	c.model = "s"
-	interfaces = append(interfaces, c)
-	for _, f := range interfaces {
-		fmt.Println(f.getName())
-	}
-	p := Plane{}
-	p.vendor = "testVendor"
-	p.model = "testModel"
-	fmt.Println(p.getName())
+	h := Human{FirstName: "yue", LastName: "yongqianng", Age: 30}
+	// fmt.Println(h)
+	hString := marshal2JsonString(h)
+	// fmt.Println(hString)
+	// fmt.Println(h)
+
+	hh := unmarshal2Struct(hString)
+	fmt.Printf("%p\n", hh)
+	// interfaces := []IF{}
+	// h := new(Human)
+	// h.firstName = "first"
+	// h.lastName = "last"
+	// interfaces = append(interfaces, h)
+	// c := new(Car)
+	// c.factory = "benz"
+	// c.model = "s"
+	// interfaces = append(interfaces, c)
+	// for _, f := range interfaces {
+	// 	fmt.Println(f.getName())
+	// }
+	// p := Plane{}
+	// p.vendor = "testVendor"
+	// p.model = "testModel"
+	// fmt.Println(p.getName())
 }
